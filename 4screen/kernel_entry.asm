7db77f00114757c49bb25ec13ab6f8dcef687e60
swordyork <sword.york@gmail.com>
2014-04-28 16:15:10 +0800

    rm 4 day

--- 4screen/kernel_entry.asm
+++
@@ -1,4 +0,0 @@
-[bits 32]
-[extern main]
-	call main
-	jmp $ 
--- 4screen/make.sh
+++
@@ -1,5 +0,0 @@
-nasm ipl.nas -o ipl.img 
-sudo mount -o loop ipl.img /run/media/ipl/
-sudo cp slef.sys /run/media/ipl/
-sudo umount /run/media/ipl 
-xxd ipl.img > ipl.hex
